b5d96a5c1d54c63a9ae0c0c7ced4b617b8588124
Shreya Prasad <shreyapd06@gmail.com>
2020-04-21 21:50:29 +0530

    Update SmallThanCurrentNumber.cpp

--- SmallThanCurrentNumber.cpp
+++ SmallThanCurrentNumber.cpp
@@ -51,3 +51,18 @@ public:
 
 //Time Complexity: O(NlogN)
 //LeetCode Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+//Explanation: 
+//So first i created a hashmap named mp with key values as different numbers in the vector nums and then the (value) part of hashmap is count of each number in the vector.
+//For ex if nums=[5,5,7] then hashmap will have 2 entries -> (key->5 value->2 ), (key->7 value 1)
+
+//After i have generated the hashmap, i am iterating the vector(in second for loop) and finding numbers smaller than each number in vector by iterating hashmap for each vector entry.
+////////////
+//for(auto &nn:mp)
+//if(nn.first!=nums[i] && nn.first<=nums[i])
+//count+=nn.second;
+//In the above code section i am iterating over hashmap and if i found any number(key of hashmap) less than and not equal to nums[i] i increase the count as i that number is what we need.
+
+////////////
+//vec.push_back(count);
+//After. calculating count i am pushing each count in a vector with its respective index.
