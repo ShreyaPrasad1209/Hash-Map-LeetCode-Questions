ad6423aef85561cd8e5d70f111588839164d5abc
Shreya Prasad <shreyapd06@gmail.com>
2020-05-04 06:16:50 +0530

    Update WordPattern.cpp

--- WordPattern.cpp
+++ WordPattern.cpp
@@ -29,4 +29,43 @@ public:
     }
 };
 
+//Simpler Implementation 
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        int j=-1;
+		//map for checking char to string
+        unordered_map<char,string> mm;
+		//map for checking string to char
+        unordered_map<string,char> mn;
+        for(int i=0;i<pattern.length();i++)
+        {
+            
+            char pp=pattern[i];
+            string s="";
+            j++;
+            while(str[j]!=' '&&j<str.length())
+            {
+                s.push_back(str[j]);
+                j++;
+            }
+            //only valid when not found in both
+            if(mm.find(pp)==mm.end()&&mn.find(s)==mn.end())
+            {
+                mm[pp]=s;
+                mn[s]=pp;
+            }
+			//if not found in only one that means word or char is being used repeatedly for another char/string combination hence reject
+            else if(mm.find(pp)==mm.end()||mn.find(s)==mn.end())
+                return false;
+				//if found but doesnt match then reject
+            else if(mm.find(pp)->second!=s||mn.find(s)->second!=pp)
+                return false;
+        }
+		//make sure j has traveled complete length of str
+        return j==str.length()?true:false;
+    }
+};
+
 //LeetCode Link: https://leetcode.com/problems/word-pattern/
